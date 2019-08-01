1443a1b009f11a024125af89b98ced9fdcfde4d0
Pom <saran.j2108@gmail.com>
2019-08-01 18:29:25 +0700

    add file

---
+++ Assets/Script/MyPrefabInstantiator.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using Vuforia;
+using System.Collections;
+public class MyPrefabInstantiator : MonoBehaviour, ITrackableEventHandler {
+  private TrackableBehaviour mTrackableBehaviour;
+    public ImageTargetBehaviour imgBehavior;
+  public Transform myModelPrefab;
+  // Use this for initialization
+  void Start ()
+  {
+    mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+    if (mTrackableBehaviour) {
+      mTrackableBehaviour.RegisterTrackableEventHandler(this);
+    }
+  }
+  // Update is called once per frame
+  void Update ()
+  {
+  }
+  public void OnTrackableStateChanged(
+    TrackableBehaviour.Status previousStatus,
+    TrackableBehaviour.Status newStatus)
+  { 
+    if (newStatus == TrackableBehaviour.Status.DETECTED ||
+        newStatus == TrackableBehaviour.Status.TRACKED ||
+        newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+    {
+      OnTrackingFound();
+    }
+  } 
+  private void OnTrackingFound()
+  {
+    Debug.Log("Tracking Found",myModelPrefab);
+        if (imgBehavior != null) {
+            Debug.Log(imgBehavior.ImageTarget.Name);
+
+        }
+
+    }
+}
